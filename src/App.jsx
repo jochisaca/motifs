58e23c63ecb075368bb8a3c50df03c5c5791870d
unknown <jochisaca@gmail.com>
2024-11-13 12:45:50 -0500

    Fixed casing issue with imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import "./Home"; // Import the Home component
-import "./ShopPage"; // Import the ShopPage component
+import Home from './Home';  // Ensure this matches the file name
+import ShopPage from `./ShopPage`; // Import the ShopPage component
 
 const App = () => {
   return (
